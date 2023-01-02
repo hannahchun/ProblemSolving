7b9e9e3edbba49b11517c8550768f9fd68499eb7
hannahchun <hannahchun2000@gmail.com>
2023-01-02 13:14:39 +0900

    [Python] Baekjoon 2920 solution1 update1

--- Baekjoon/2920_sol1.py
+++ Baekjoon/2920_sol1.py
@@ -5,20 +5,15 @@
 num = input()
 num_list = num.split(' ')
 
-isAscending = True # Assume the numbers are in an ascending order
+isAscending = True 
+isDescending = True 
 
-# Check if the numbers are in an ascending order
 for i in range(1, len(num_list)) :
     if num_list[i] < num_list[i-1] : 
         isAscending = False
-
-isDescending = True # Assume the numbers are in an descending order
-
-# Check if the numbers are in a descending order
-for i in range(1, len(num_list)) :
     if num_list[i-1] < num_list[i] :
         isDescending = False
-
+        
 if isAscending == True :
     print("ascending")
 elif isDescending == True :
