b45ab2d6aa3130d11460473c53f50a1f20712486
hannahchun <80413027+hannahchun@users.noreply.github.com>
2023-01-24 16:50:19 +0900

    [C++] Leetcode 110 solution1 update1

--- Leetcode/110.cpp
+++ Leetcode/110.cpp
@@ -19,39 +19,40 @@ struct TreeNode {
 class Solution {
     public :
         bool isBalanced(TreeNode *root) {
-            if (root == NULL)
-                return true;
-            return Max_depth(root);
+            bool res = true;
+            getDepth(root, res);
+            return res;
         }
-        int Max_depth(TreeNode *head) {
-            if (head == NULL)
+        int getDepth(TreeNode *root, bool &res) {
+            if(!res) // break recursion
                 return 0;
-            int left = Max_depth(head->left);
-            int right = Max_depth(head->right);
-            if (abs(left-right) > 1)
-                return false;
+            if (root == NULL)
+                return 0;
+            int left = getDepth(root->left, res); // depth of left sub tree
+            int right = getDepth(root->right, res); // depth of right sub tree
+            if (abs(left - right) > 1)
+                res = false;
             return max(left, right) + 1;
         }
 };
 
 int main() {
-    
+    /*
     TreeNode *root = new TreeNode(3);
     root->left = new TreeNode(9);
     root->right=new TreeNode(20);
     root->right->left=new TreeNode(15);
     root->right->right=new TreeNode(7);
+    */
     
-    /*
     TreeNode *root = new TreeNode(1);
     root->left = new TreeNode(2);
     root->left->left = new TreeNode(3);
     root->left->left->left = new TreeNode(4);
-    root->left->left->right = new TreeNode(4);
-    root->left->right = new TreeNode(3);
-    root->right=new TreeNode(2);
-    */
-   
+    root->right = new TreeNode(2);
+    root->right->right = new TreeNode(3);
+    root->right->right->right = new TreeNode(4);
+    
     Solution sol1;
 
     if(sol1.isBalanced(root))
