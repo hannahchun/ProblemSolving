52ee1f3b0776b8a6fcb54b5c35489ca274db4913
hannahchun <80413027+hannahchun@users.noreply.github.com>
2023-09-07 13:32:44 +0900

    [Python] Leetcode 338 solution2

--- Leetcode/338.py
+++ Leetcode/338.py
@@ -1,4 +1,7 @@
 # https://leetcode.com/problems/counting-bits/
+
+"""
+# O(nlogn)
 # Solution 1 - Brute Force
 # Python
 
@@ -13,6 +16,21 @@ class Solution(object):
                     results[i] += 1
                 j = j // 2 
         return results
+"""
+# O(n)
+# Solution 2 - Dynamic Programming
+# Python
+
+class Solution(object):
+    def countBits(self, n):
+        dp = [0] * (n + 1)
+        offset = 1
+        
+        for i in range(1, n + 1) :
+            if offset * 2 == i :
+                offset = i
+            dp[i] = dp [i - offset] + 1
+        return dp
 
 sol1=Solution()
 print(Solution.countBits(sol1, 2))
