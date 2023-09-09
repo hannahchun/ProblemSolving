67e5833b1c02587640802ce327dc79b67b3ab887
hannahchun <80413027+hannahchun@users.noreply.github.com>
2023-09-09 17:28:16 +0900

    [Python] Baekjoon 17216 solution1

---
+++ Baekjoon/17216.py
@@ -0,0 +1,15 @@
+# https://www.acmicpc.net/problem/17216
+# Solution 1
+# Python
+
+import copy
+
+n = int(input())
+arr = list(map(int, input().split()))
+dp = copy.deepcopy(arr)
+
+for i in range(n) :
+    for j in range(i) :
+        if arr[i] < arr[j] :
+            dp[i] = max(dp[i], dp[j] + arr[i])
+print(max(dp))
